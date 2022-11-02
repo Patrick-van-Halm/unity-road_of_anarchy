a1ed42213be91e860656faf27ca507e9545d0cf9
Isabel Pijnenburg <433195@student.fontys.nl>
2022-11-02 23:31:01 +0100

    simple save load system

--- Assets/Scripts/ScriptableObjects/GameSettings.cs
+++ Assets/Scripts/ScriptableObjects/GameSettings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "GameSettings")]
@@ -18,4 +19,22 @@ public class GameSettings : ScriptableObject
 
     [Header("Gunner settings")]
     public bool AutomaticReload;
+
+    private string _jsonPath => Application.dataPath + "/" + name + ".txt";
+
+    internal void Save()
+    {
+        string settings = JsonUtility.ToJson(this, true);
+        File.WriteAllText(_jsonPath, settings);
+    }
+
+    internal void Load()
+    {
+        if (File.Exists(_jsonPath))
+        {
+            string settingsText = File.ReadAllText(_jsonPath);
+            JsonUtility.FromJsonOverwrite(settingsText, this);
+        }
+    }
+
 }
