ffce27dea376efc00f5c7ed2c7b3250b201b84bd
Lengton,Robbert R.C <r.lengton@student.fontys.nl>
2022-12-19 14:01:20 +0000

    Request to merge 'feature/lobby-list' into 'develop'

--- Assets/Scripts/UI/LobbyListingRowUI.cs
+++ Assets/Scripts/UI/LobbyListingRowUI.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LobbyListingRowUI : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _lobbyNameTbx;
+    [SerializeField] private TMP_Text _lobbyPlayerCountTbx;
     public LobbyDetails Details;
+
+    private void Start()
+    {
+        _lobbyNameTbx.text = Details.Name;
+        _lobbyPlayerCountTbx.text = Details.PlayerCount.ToString();
+    }
 
     public void Connect()
     {
